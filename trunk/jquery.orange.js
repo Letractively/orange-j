7edf41693ae9e333651b02290071ebb9fad66c27
donovan.walker@gmail.com <donovan.walker@gmail.com@84c854d2-885a-11de-b760-4d83c623b0ac>
2011-05-14 05:02:19 +0000

    Feature updates.

--- trunk/jquery.orange.js
+++ trunk/jquery.orange.js
@@ -2,7 +2,7 @@
  * Projekt Orange - Orange-J -
  * Orange Extension for jQuery or Standalone
  * Bringing even more advanced coding laziness to the developer
- * Version 2.4 beta 3
+ * Version 2.4 beta 4
  * author: Donovan Walker
  */
 
@@ -82,15 +82,17 @@ function Snippet(inString, inLib, inParent) {
    //defaults
    this.isSnippet      = true;
    this.config         = {
+      caseConvert:false,
       chopTo:false, // = maxlen - maxchop initialized by maxlen
       collapsewhite:false,
+      dateFormat:false,
       htmlentities:false,
       maxend:"",
       maxchop:0,
       maxlen:false,
       maxnohtml:false,
       maxwords:false,
-      caseConvert:false
+      numberFormat:false
    };
    this.cycleInc       = 0;
    this.cycleName      = false;
@@ -457,6 +459,32 @@ Snippet.prototype.fill = function(obj) {
          } else if(this.config.htmlentities) {
             obj = this.htmlentities(obj);
          }
+         if(this.config.numberFormat) {
+            obj = obj.toString().split('.');
+            if(obj[0].charAt(0) == '-') {
+               out = '-';
+               obj[0] = obj[0].substring(1);
+            }
+            out += this.config.numberFormat.intMask.substring(0, (this.config.numberFormat.intMask.length - obj[0].length)) + obj[0];
+            if(this.config.numberFormat.precisionMask) {
+               if(obj.hasOwnProperty(1)) {
+                  out += '.' + (obj[1].substring(0, this.config.numberFormat.precisionMask.length) + this.config.numberFormat.precisionMask).substring(0, this.config.numberFormat.precisionMask.length);
+               } else {
+                  out += '.' + this.config.numberFormat.precisionMask;
+               }
+            }
+            obj = out;
+         }
+         
+         if(this.config.dateFormat) {
+            if(! isNaN (obj-0)) {
+               this.dateConverter.setTime(obj);
+            } else {
+               this.dateConverter.setTime(Date.parse(obj));
+            }
+            obj = this.dateConverter.format(this.config.dateFormat);
+         }
+
          if(typeof this.config.caseConvert == 'string') {
             switch(this.config.caseConvert) {
                case 'uppercase' :
@@ -728,6 +756,30 @@ Snippet.prototype.parseConfig = function(inString) {
             this.cycleValues = temp.split("|");
             inString = inString.replace("cycleName=" + this.cycleName + " " + temp + "", "");
          }
+         //Number format not yet functional.
+         index = inString.indexOf("numberFormat=\"");
+         if(index > -1) {
+            temp = inString.substring(index + 14);
+            temp = temp.substring(0, temp.indexOf("\""));
+            inString = inString.replace("numberFormat=" + temp, "");
+            temp = temp.split(".");
+           this.config.numberFormat = {intMask:temp[0], precisionMask:false};
+           if(temp.length > 1) {
+              this.config.numberFormat.precisionMask = temp[1];
+           }
+         }
+         
+         index = inString.indexOf("dateFormat=\"");
+         if(index > -1) {
+            temp = inString.substring(index + 12);
+            temp = temp.substring(0, temp.indexOf("\""));
+            if(typeof Date.prototype.format == 'function') {
+               this.config.dateFormat = temp;
+               this.dateConverter = new Date();
+            }
+            inString = inString.replace("dateFormat=" + temp, "\"");
+         }
+         
          index = inString.indexOf("maxlen=");
          if(index > -1) {
             temp = inString.substring(index + 7);
@@ -790,6 +842,7 @@ Snippet.prototype.parseConfig = function(inString) {
             this.config.caseConvert = 'lowercase';
             inString = inString.replace("lowercase", "");
          }
+
    }
 }
 
@@ -1655,3 +1708,76 @@ if(typeof(jQuery) == "function") {
 } else {
    sl = new SnippetLib();
 }
+
+//
+
+// Simulates PHP's date function. Credit to Jac Wright & contributors http://jacwright.com/projects/javascript/date_format
+Date.prototype.format = function(format) {
+	var returnStr = '';
+	var replace = Date.replaceChars;
+	for (var i = 0; i < format.length; i++) {
+		var curChar = format.charAt(i);
+		if (i - 1 >= 0 && format.charAt(i - 1) == "\\") {
+			returnStr += curChar;
+		}
+		else if (replace[curChar]) {
+			returnStr += replace[curChar].call(this);
+		} else if (curChar != "\\"){
+			returnStr += curChar;
+		}
+	}
+	return returnStr;
+};
+
+Date.replaceChars = {
+	shortMonths: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+	longMonths: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+	shortDays: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+	longDays: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+
+	// Day
+	d: function() { return (this.getDate() < 10 ? '0' : '') + this.getDate(); },
+	D: function() { return Date.replaceChars.shortDays[this.getDay()]; },
+	j: function() { return this.getDate(); },
+	l: function() { return Date.replaceChars.longDays[this.getDay()]; },
+	N: function() { return this.getDay() + 1; },
+	S: function() { return (this.getDate() % 10 == 1 && this.getDate() != 11 ? 'st' : (this.getDate() % 10 == 2 && this.getDate() != 12 ? 'nd' : (this.getDate() % 10 == 3 && this.getDate() != 13 ? 'rd' : 'th'))); },
+	w: function() { return this.getDay(); },
+	z: function() { var d = new Date(this.getFullYear(),0,1); return Math.ceil((this - d) / 86400000); }, // Fixed now
+	// Week
+	W: function() { var d = new Date(this.getFullYear(), 0, 1); return Math.ceil((((this - d) / 86400000) + d.getDay() + 1) / 7); }, // Fixed now
+	// Month
+	F: function() { return Date.replaceChars.longMonths[this.getMonth()]; },
+	m: function() { return (this.getMonth() < 9 ? '0' : '') + (this.getMonth() + 1); },
+	M: function() { return Date.replaceChars.shortMonths[this.getMonth()]; },
+	n: function() { return this.getMonth() + 1; },
+	t: function() { var d = new Date(); return new Date(d.getFullYear(), d.getMonth(), 0).getDate() }, // Fixed now, gets #days of date
+	// Year
+	L: function() { var year = this.getFullYear(); return (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)); },	// Fixed now
+	o: function() { var d  = new Date(this.valueOf());  d.setDate(d.getDate() - ((this.getDay() + 6) % 7) + 3); return d.getFullYear();}, //Fixed now
+	Y: function() { return this.getFullYear(); },
+	y: function() { return ('' + this.getFullYear()).substr(2); },
+	// Time
+	a: function() { return this.getHours() < 12 ? 'am' : 'pm'; },
+	A: function() { return this.getHours() < 12 ? 'AM' : 'PM'; },
+	B: function() { return Math.floor((((this.getUTCHours() + 1) % 24) + this.getUTCMinutes() / 60 + this.getUTCSeconds() / 3600) * 1000 / 24); }, // Fixed now
+	g: function() { return this.getHours() % 12 || 12; },
+	G: function() { return this.getHours(); },
+	h: function() { return ((this.getHours() % 12 || 12) < 10 ? '0' : '') + (this.getHours() % 12 || 12); },
+	H: function() { return (this.getHours() < 10 ? '0' : '') + this.getHours(); },
+	i: function() { return (this.getMinutes() < 10 ? '0' : '') + this.getMinutes(); },
+	s: function() { return (this.getSeconds() < 10 ? '0' : '') + this.getSeconds(); },
+	u: function() { var m = this.getMilliseconds(); return (m < 10 ? '00' : (m < 100 ?
+'0' : '')) + m; },
+	// Timezone
+	e: function() { return "Not Yet Supported"; },
+	I: function() { return "Not Yet Supported"; },
+	O: function() { return (-this.getTimezoneOffset() < 0 ? '-' : '+') + (Math.abs(this.getTimezoneOffset() / 60) < 10 ? '0' : '') + (Math.abs(this.getTimezoneOffset() / 60)) + '00'; },
+	P: function() { return (-this.getTimezoneOffset() < 0 ? '-' : '+') + (Math.abs(this.getTimezoneOffset() / 60) < 10 ? '0' : '') + (Math.abs(this.getTimezoneOffset() / 60)) + ':00'; }, // Fixed now
+	T: function() { var m = this.getMonth(); this.setMonth(0); var result = this.toTimeString().replace(/^.+ \(?([^\)]+)\)?$/, '$1'); this.setMonth(m); return result;},
+	Z: function() { return -this.getTimezoneOffset() * 60; },
+	// Full Date/Time
+	c: function() { return this.format("Y-m-d\\TH:i:sP"); }, // Fixed now
+	r: function() { return this.toString(); },
+	U: function() { return this.getTime() / 1000; }
+};
